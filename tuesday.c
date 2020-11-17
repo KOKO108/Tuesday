611f4434f39b36b7694f2790d8dc2f4fd28e6cbe
wyd645023 <74590195+wyd645023@users.noreply.github.com>
2020-11-17 16:25:10 +0800

    Update tuesday.c

--- tuesday.c
+++ tuesday.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 
 void main(){
-	printf("Today is tuesday!");
+	printf("Today is tuesday!/n");
+}
+
+void Monday(){
+	printf("The day is first day in a week!And before Tuesday!/n");
 }
